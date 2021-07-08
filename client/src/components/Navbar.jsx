4951645580f52bb9de3133bf1b1ab67b85e1dce3
William Perry <durabilliam@gmail.com>
2021-07-08 11:01:38 -0230

    add navbar css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,6 +6,11 @@ import './Navbar.css'
 
 const Logo = require('../Images/Issue-252.png').default
 const Supes = require('../Images/superman.png').default
+const Spidey = require('../Images/spider-man.png').default
+const Spawn = require('../Images/spawn.png').default
+const Other = require('../Images/other.png').default
+
+
 
 
 export default function ComixsNavbar(props) {
@@ -25,7 +30,10 @@ export default function ComixsNavbar(props) {
         <Navbar.Brand href="/">
         <div className='logo'><img src={Logo} id='logopic' width="75em" /></div>
         </Navbar.Brand>
-        <div classname='supes'><img src={Supes} id='supes'/></div>
+        <div classname='supes'><img src={Supes} id='supes' height="100em"/></div>
+        <div classname='spidey'><img src={Spidey} id='spidey' height="100em"/></div>
+        <div classname='spawn'><img src={Spawn} id='spawn' height="100em"/></div>
+        <div classname='other'><img src={Other} id='other' height="100em"/></div>
         <div id="rightNav">
           <Link to='/login'>Log In</Link>
           <br></br>
@@ -41,14 +49,21 @@ export default function ComixsNavbar(props) {
           <Navbar.Brand href="/">
           <div className='logo'><img src={Logo} id='logopic' width="75em" /></div>
           </Navbar.Brand>
-          <div classname='supes'><img src={Supes} id='supes'width="75em"/></div>
-              <div id="rightNav">
-                <a>Thanks for Logging in {user.full_name}</a>
-                <br></br>
-                {/* <a href={`/favourites`}><img src={favouriteStamp} className="favourite-image" height="25" width="50"></img></a> */}
-                <a class="logout-button" onClick={userLogout}>Logout</a>
-              </div>
-            </Navbar>
+          <div id="topNav">
+          <div id="publishers">
+            <div classname='supes'><img src={Supes} id='supes' height="100em"/></div>
+            <div classname='spidey'><img src={Spidey} id='spidey' height="100em"/></div>
+            <div classname='spawn'><img src={Spawn} id='spawn' height="100em"/></div>
+            <div classname='other'><img src={Other} id='other' height="100em"/></div>
+          </div>
+          <div id="rightNav">
+             <a>Thanks for Logging in {user.full_name}</a>
+             <br></br>
+             {/* <a href={`/favourites`}><img src={favouriteStamp} className="favourite-image" height="25" width="50"></img></a> */}
+             <a class="logout-button" onClick={userLogout}>Logout</a>
+          </div>
+          </div>
+          </Navbar>
         </div>
       )
     }
